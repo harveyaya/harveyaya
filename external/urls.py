3c08318e306cbbbef43fc2249226f562b9dface4
Yu Hou <harveyaya@Harveys-MacBook-Pro-2.local>
2018-02-20 22:53:43 -0500

    Added Resume link on menu tab

--- external/urls.py
+++ external/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 from views import (
     EnterPageView,
     HomePageView,
+    ResumePDFVIew,
 )
 
 urlpatterns = [
     url(r'^$', EnterPageView.as_view(), name="enter_page"),
     url(r'^home/$', HomePageView.as_view(), name="external_homepage"),
+    url(r'^resume/$', ResumePDFVIew.as_view(), name="resume"),
 ]
--- external/views.py
+++ external/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.views.generic import View
+from django.http import FileResponse, HttpResponse
 
 class EnterPageView(View):
     template_name = "enter_page.html";
@@ -13,3 +14,10 @@ class HomePageView(View):
     def get(self, request):
         context = {}
         return render(request, self.template_name, context)
+
+class ResumePDFVIew(View):
+    def get(self,request):
+        return self.pdf_view(request)
+
+    def pdf_view(self, request):
+        return FileResponse(open("./site_static/res/Yu_Hou_Resume.pdf", "r"), content_type='application/pdf')
